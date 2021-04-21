5bd4da14457f239db02a20fe511883d4413457d4
Pol Ajazi <pol.ajazi@yahoo.com>
2021-04-21 05:51:32 -0400

    wrong prod pord

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl: 'http://100.27.49.189:31772/SurveyMiddleware/api',
+    apiUrl: 'http://18.215.165.20:31772/SurveyMiddleware/api',
     production: true,
 };
